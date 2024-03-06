84fed23a6c74eece630c36939f555e958dbd6b37
kafelllll <nastasencik@gmail.com>
2024-03-06 19:23:58 +0200

    fixed

--- lab2/Main.java
+++ lab2/Main.java
@@ -2,18 +2,18 @@ package lab2;
 
 public class Main {
     public static void main(String[] args) {
-        double ширина = Double.parseDouble(System.console().readLine("Enter the length of the rectangle: "));
-        double висота = Double.parseDouble(System.console().readLine("Enter the width of the rectangle: "));
-        double верх = Double.parseDouble(System.console().readLine("Enter the first leg of the right triangle: "));
-        double низ = Double.parseDouble(System.console().readLine("Enter the second leg of the right triangle: "));
+        double width = Double.parseDouble(System.console().readLine("Enter the length of the rectangle: "));
+        double height = Double.parseDouble(System.console().readLine("Enter the width of the rectangle: "));
+        double top = Double.parseDouble(System.console().readLine("Enter the first leg of the right triangle: "));
+        double bottom = Double.parseDouble(System.console().readLine("Enter the second leg of the right triangle: "));
         
-        Shape قمة = new Rectangle(ширина, висота);
-        Shape قم = new Triangle(верх, низ);
+        Shape rec = new Rectangle(width, height);
+        Shape tri = new Triangle(top, bottom);
         
-        double rectangleArea = قمة.getArea();
-        double triangleArea = قم.getArea();
+        double rectangleArea = rec.getArea();
+        double triangleArea = tri.getArea();
 
         System.out.println("Area of ​​the rectangle: " + rectangleArea);
-        System.out.println("Площа прямокут قمного трикутника: " + triangleArea);
+        System.out.println("Area of ​​the triangle: " + triangleArea);
     }
 }
