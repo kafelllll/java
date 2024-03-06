b8dbd8832da42127bda1e3a390b1ec6711f76734
kafelllll <nastasencik@gmail.com>
2024-03-06 19:30:27 +0200

    ключ

--- labD.java
+++ labD.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class labD {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("Кеу 1", "1");
