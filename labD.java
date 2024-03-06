30bb5cb54cea5e7e4dc132de101f5bb17c58726d
kafelllll <nastasencik@gmail.com>
2024-03-06 19:29:13 +0200

    fi

--- labD.java
+++ labD.java
@@ -4,9 +4,9 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
-        map.put("Ключ 1", "1");
-        map.put("Ключ 2", "2");
-        map.put("Ключ 3", "3");
+        map.put("Кеу 1", "1");
+        map.put("Кеу 2", "2");
+        map.put("Кеу 3", "3");
 
         System.out.println(map.values());
     }
